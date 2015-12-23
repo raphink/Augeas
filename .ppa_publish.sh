67ba036b6e76813072254dffa3668aab8f276114
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-12-23 10:23:52 +0100

    Add dput, add debian/

--- .ppa_publish.sh
+++ .ppa_publish.sh
@@ -2,11 +2,6 @@
 openssl aes-256-cbc -K $encrypted_d53479e3bcc6_key -iv $encrypted_d53479e3bcc6_iv -in autopackage.gpg.enc -out autopackage.gpg -d
 gpg --import autopackage.gpg
 
-dget https://launchpad.net/~raphink/+archive/ubuntu/augeas/+files/augeas_1.4.0-0ubuntu1~augeas2.dsc
-dpkg-source -x augeas_1.4.0-0ubuntu1~augeas2.dsc
-mv augeas-1.4.0/debian .
-rm -rf augeas-1.4.0
-
 export NAME="Travis Autopackage"
 export EMAIL="raphink+travisautopackage@gmail.com"
 echo -e "\n" | dch -v 1.4.0+$(date +'%Y%m%d')+${TRAVIS_BUILD_NUMBER} -D trusty "Autobuild for ${TRAVIS_COMMIT}"

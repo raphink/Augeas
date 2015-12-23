61deeed65d4cb0d0d6059fa47c2cfe0f77bd3202
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-12-23 11:16:08 +0100

    Create orig.tar.gz

--- .ppa_publish.sh
+++ .ppa_publish.sh
@@ -6,7 +6,9 @@ gpg --import autopackage.gpg
 
 export NAME="Travis Autopackage"
 export EMAIL="raphink+travisautopackage@gmail.com"
-echo -e "\n" | dch -v 1.4.0+$(date +'%Y%m%d')+${TRAVIS_BUILD_NUMBER} -D trusty "Autobuild for ${TRAVIS_COMMIT}"
+PKG_VERSION="1.4.0+$(date +'%Y%m%d')+${TRAVIS_BUILD_NUMBER}"
+tar --exclude debian --exclude-vcs -czf  "../augeas_${PKG_VERSION}.orig.tar.gz" .
+echo -e "\n" | dch -v "${PKG_VERSION}" -D trusty "Autobuild for ${TRAVIS_COMMIT}"
 
 debuild -S -sa
 

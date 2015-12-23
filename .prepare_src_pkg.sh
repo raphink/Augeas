812b74a07fbe5ac875a08a719bfd94a1a44a42a9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-12-23 11:55:32 +0100

    PKG_VERSION

--- .prepare_src_pkg.sh
+++ .prepare_src_pkg.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y devscripts gnupg dput dh-autoreconf
 
 export NAME="Travis Autopackage"
 export EMAIL="raphink+travisautopackage@gmail.com"
+PKG_VERSION="1.4.0+$(date +'%Y%m%d')+${TRAVIS_BUILD_NUMBER}"
 tar --exclude debian --exclude-vcs -czf  "../augeas_${PKG_VERSION}.orig.tar.gz" .
 
 echo -e "\n" | dch -v "${PKG_VERSION}" -D trusty "Autobuild for ${TRAVIS_COMMIT}"

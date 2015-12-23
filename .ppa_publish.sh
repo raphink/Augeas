3da6ef58990334ada6a327ef588e66dccbbcfe1f
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-12-23 10:38:20 +0100

    sudo required to install packages

--- .ppa_publish.sh
+++ .ppa_publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo apt-get install -y devscripts gnupg dput
+
 openssl aes-256-cbc -K $encrypted_d53479e3bcc6_key -iv $encrypted_d53479e3bcc6_iv -in autopackage.gpg.enc -out autopackage.gpg -d
 gpg --import autopackage.gpg
 

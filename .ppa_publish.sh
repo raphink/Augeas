6a45febac8a873841951fd6711b68bd77ca38118
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-12-23 11:00:33 +0100

    Add dh-autoreconf

--- .ppa_publish.sh
+++ .ppa_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install -y devscripts gnupg dput
+sudo apt-get install -y devscripts gnupg dput dh-autoreconf
 
 openssl aes-256-cbc -K $encrypted_d53479e3bcc6_key -iv $encrypted_d53479e3bcc6_iv -in autopackage.gpg.enc -out autopackage.gpg -d
 gpg --import autopackage.gpg
